3cfcb1f34e09bc435e97d88c9ff0c62c30381342
Sunny Kim <starwin@gmail.com>
2022-09-26 12:56:30 +0900

    camera control

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CameraControl : MonoBehaviour {
+    [Header("Settings")]
+    public float distance = 5f;
+    public float height = 2.5f;
+    public float panSpeed = 90f;
+
+    private float panAngle;
+    private Transform player;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.forward = player.transform.position - transform.position;
+        panAngle = transform.rotation.eulerAngles.y;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        UpdateInput();
+        Quaternion lookdir = Quaternion.Euler(0, panAngle, 0);
+        transform.position = player.transform.position - (lookdir * Vector3.forward) * distance + Vector3.up * height;
+
+        transform.forward = player.transform.position - transform.position;
+    }
+
+    private void UpdateInput() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            panAngle += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            panAngle -= panSpeed * Time.deltaTime;
+        }
     }
 }
