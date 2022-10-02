03cf34c2ced47e63618a73bc99be03b3f4d47ae2
yongin0814 <yongin0814@gmail.com>
2022-10-02 18:52:06 +0900

    Revert "Revert "0926_HW""
    This reverts commit 222adcf446b5e630f1aa6ee8fd647fdde16b16dd.

---
+++ Assets/00_YJ/04_Scripts/GetDataFromSO.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 각 item에 붙을거다 
+
+public class GetDataFromSO : MonoBehaviour
+{
+
+    // data 값 받아오기
+    public ItemData itemData;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject temp = collision.gameObject; // 일단 충돌체 정보 받아주고
+        print("충돌체 정보" + temp);
+
+        if (temp.name.Contains("Player")) // 만약 충돌체가 player이면 
+        {
+
+            //각 함수들 실행 해주고 
+            HitDamage();
+            Speed();
+            VFX();
+
+            // 0.5초 뒤에 이 게임오브젝트 없애주기 
+            Destroy(this.gameObject, 0.5f);
+
+        }
+    }
+
+    void HitDamage() // Fireball의 Hit Damage 바꿔주기
+    {
+        FireBall.hitDamage = itemData.HitDamage;
+    }
+
+    void Speed() // player의 speed 바꿔주기 
+    {
+        PlayerControl.moveSpeed = itemData.Speed;
+    }
+
+    void VFX() // 이 VFX Instantiate 해주기
+    {
+        Instantiate(itemData.vfx, this.gameObject.transform);
+    }
+
+}
